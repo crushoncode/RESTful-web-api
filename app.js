0a8d1dd974b6d6222f4c85e42be412830d30b377
Serina Ko <34416116+crushoncode@users.noreply.github.com>
2018-05-31 14:44:57 +1000

    display Hello World

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/bookstore')
+const db = mongoose.connection;
+
+app.get('/', function(req, res){
+    res.send('Hello World!');
+}); 
+
+app.listen(3000);
+console.log('Running on port 3000...')
